1dd6bc065013702301d6cc8d59c3ce5459df7b62
doron <doronn89@gmail.com>
2022-12-28 00:55:44 +0200

    Added input system and made a connection for controlling the player

---
+++ Assets/PathOfTheMachine/Scripts/PlayerController/PlayerInputSubscribers/PlayerInputListenerForPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PathOfTheMachine.Scripts.PlayerController.PlayerInputSubscribers
+{
+    public class PlayerInputListenerForPlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private global::Scripts.PlayerController.Platformer.PlayerController _playerController;
+
+        public void Jump(InputAction.CallbackContext context)
+        {
+            if (!context.ReadValueAsButton())
+            {
+                return;
+            }
+            
+            _playerController.RequestJump();
+        }
+        
+        public void HorizontalMovement(InputAction.CallbackContext context)
+        {
+            _playerController.SetHorizontalInput(context.ReadValue<float>());
+        }
+    }
+}
